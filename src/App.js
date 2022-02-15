b4d9940b76114dd12d4656b974df5fa89d43d80c
NickBwalley <nickbwalley@gmail.com>
2022-02-15 23:47:31 +0300

    made some adjustments reactjs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import Map from './components/Map/Map';
 const App = () => {
     const [places, setPlaces] = useState([]);
 
+    const [coordinates, setCoordinates] = useState({lat: 0, lng: 0});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} }) => {
+
+        })
+    });
+
     useEffect(() => {
         getPlacesData()
             .then((data) => {
@@ -24,7 +33,11 @@ const App = () => {
                         <List/>
                     </Grid>
                     <Grid item xs={12} md={8}>
-                        <Map/>
+                        <Map
+                            setCoordinates = {setCoordinates}
+                            setBounds = {setBounds}
+                            coordinates = {coordinates}
+                        />
                     </Grid>
 
                 </Grid>
